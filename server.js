f2cceb7dbd42b9eb13c050d0feee736367bc4913
Kirill <ykirillm@mail.ru>
2024-07-28 17:24:20 +0300

    add cors

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const { dotenv, cors } = require('./src/config/config')
 const routes = require('./src/routes/routes')
 
 const app = express()
+const corsOptions = {
+    origin: 'http://87.228.12.128', // Разрешить запросы с этого IP-адреса
+};
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/', routes)
 
