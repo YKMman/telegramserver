392cf737084b54c7cd3c2b2f41f0afadea667497
Kirill <ykirillm@mail.ru>
2024-08-01 18:22:02 +0300

    heh

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+// const express = require('express')
+// const path = require('path')
+// const bodyParser = require('body-parser')
 
-const { dotenv, cors } = require('./src/config/config')
-const routes = require('./src/routes/routes')
+// const { dotenv, cors } = require('./src/config/config')
+// const routes = require('./src/routes/routes')
 
 
+// const app = express();
 
-// const clientPath = path.join(__dirname, '../testing/dist');
+// app.use(express.json());
+// app.use(cors());
+
+// app.use('/', routes)
+// app.listen(process.env.PORT, () => {
+//     console.log(`app listening on ${process.env.PORT}`)
+// })
+
+
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { dotenv, cors } = require('./src/config/config');
+const routes = require('./src/routes/routes');
+
 const app = express();
 
-// Сервирование статичных файлов из папки dist
-// app.use(express.static(clientPath));
 app.use(express.json());
-
-// Настройка CORS
-// const corsOptions = {
-//     origin: 'https://testing-eight-nu.vercel.app', // Разрешить запросы только с этого домена
-//     optionsSuccessStatus: 200 // Некоторые легаси браузеры (IE11, различные SmartTV) читают статус 204 как "no content"
-// };
-  
 app.use(cors());
 
+app.use('/', routes);
 
+const options = {
+  key: fs.readFileSync('/etc/ssl/certs/privkey.pem'),
+  cert: fs.readFileSync('/etc/ssl/certs/fullchain.pem')
+};
 
-// Обработка всех остальных запросов и отправка index.html
-// app.get('/', (req, res) => {
-//     console.log(path.join(__dirname, '../testing/dist'), 'index.html')
-//     res.sendFile(path.join(clientPath, 'index.html'));
-// });
-  
+https.createServer(options, app).listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-app.use('/', routes)
-
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`app listening on ${process.env.PORT}`)
-})
 
